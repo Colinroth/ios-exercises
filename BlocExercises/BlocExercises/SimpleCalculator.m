e924f3e1f66098a29dd80044b44ed93ee0fe3136
Colin Roth <colin@getwoozy.com>
2015-02-09 21:07:32 -0500

    Completed numbers and variables/updated WizardOfOz

--- BlocExercises/BlocExercises/SimpleCalculator.m
+++ BlocExercises/BlocExercises/SimpleCalculator.m
@@ -10,19 +10,33 @@
 
 @implementation SimpleCalculator
 
-- (NSInteger) increaseNumberBy1:(NSInteger) number {
+- (NSInteger) increaseNumberBy1:(NSInteger) number  {
     /* WORK HERE */
-    return number;
+    NSInteger resultNumber = number + 1;
+    
+    return resultNumber;
+    // return number+1;
 }
 
 - (NSInteger) addNumber:(NSInteger) number1 toNumber:(NSInteger) number2 {
     /* WORK HERE */
-    return number1;
+    
+    NSInteger resultNumber = number1 + number2;
+    
+    return resultNumber;
 }
 
 - (NSInteger) remainderOfNumber:(NSInteger) dividend dividedByNumber:(NSInteger) divisor {
     /* WORK HERE */
-    return 0;
+    
+    NSInteger resultNumber;
+
+    resultNumber = dividend % divisor;
+ 
+    //NSInteger divisionResult = dividend / divisor;
+    //resultNumber = (dividend - (divisor * divisionResult));
+    
+    return resultNumber;
 }
 
 @end
